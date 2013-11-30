0aea086f2033554dbad097d65dd9489f4868fce8
Adam Tumminaro <adam@tumminaro.com>
2013-11-30 12:40:02 -0600

    Initial Model Add

---
+++ Data/ShipB.SpeakOutCA.Data/Models/CategoryGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipB.SpeakOutCA.Data.Models
+{
+    public class CategoryGroup
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
---
+++ Data/ShipB.SpeakOutCA.Data/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipB.SpeakOutCA.Data.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string SecondaryEmail { get; set; }
+        public string Phone { get; set; }
+        public string SecondaryPhone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+    }
+}
